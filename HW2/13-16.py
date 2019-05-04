2c888f8da1dd9464a299682510e13bd2510df89a
andy94077 <andy94077@gmail.com>
2019-05-04 21:01:38 +0800

    [hw2] finish

--- HW2/13-16.py
+++ HW2/13-16.py
@@ -1,5 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+def read_data(filename):
+	'''@return: (array, array). X, Y'''
+	data = np.loadtxt(filename,dtype=np.float64)
+	return data[:,:-1], data[:, -1]
+
 def draw(title=None, xlabel=None, ylabel=None, has_legend=False,savefig=False):
 	if title is not None:
 		plt.title(title)
@@ -15,25 +20,22 @@ def draw(title=None, xlabel=None, ylabel=None, has_legend=False,savefig=False):
 		plt.savefig(savefig)
 	plt.close()
 
-def get_g(data, u):
+def get_g(X, Y, u):
+	'''@return (tuple, float, float), (g, ein, epsilon)'''
 	min_g = (-1, 0, -np.inf)
 	min_err_sum = np.sum(u)
-	data=data.copy()
-	for i in range(data.shape[1] - 1):  #exclude Y
-		order=data[:, i].argsort()
-		data = data[order]  #sort by the ith feature
-		u = u[order]
-		Xi = data[:,i]
-		Y = data[:, -1]
+	for i in range(X.shape[1]):
+		order = X[:, i].argsort()
+		Xi = X[order,i]
 		
 		thetas = np.concatenate(([-np.inf], (Xi[:-1] + Xi[1:]) / 2))
 		for s in [-1, 1]:
 			for theta in thetas:
-				err_sum = u@(g_func((s,i,theta),data)!=Y)
+				err_sum = u@(g_func((s,i,theta), X)!=Y)
 				if err_sum < min_err_sum:
 					min_err_sum = err_sum
 					min_g = (s, i, theta)
-	return min_g, min_err_sum / data.shape[0], min_err_sum/np.sum(u)
+	return min_g, np.mean(g_func(min_g, X) != Y), min_err_sum / np.sum(u)
 
 def g_func(tup,X):
 	s, i, theta = tup
@@ -43,23 +45,21 @@ def G_func(G, X):
 	return np.sign(np.sign(X[:, G[:, 1].astype(int)] - G[:, 2]) @ G[:, 0:1]).flatten()
 
 def err_rate(X, Y, G):
-	return np.float(np.sum(G_func(G, X) != Y)) / X.shape[0]
+	return np.mean(G_func(G, X) != Y)
 
 if __name__ == "__main__":
-	train = np.loadtxt('hw2_adaboost_train.dat', dtype=np.float64)
-	trainX, trainY = train[:,:-1], train[:, -1]
-	test = np.loadtxt('hw2_adaboost_test.dat', dtype=np.float64)
-	testX, testY = test[:,:-1], test[:, -1]
+	trainX, trainY = read_data('hw2_adaboost_train.dat')
+	testX, testY = read_data('hw2_adaboost_test.dat')
 
 	T = 300
 	Ein = []
 	G = np.zeros((T,3)) #[s, i, theta]
 	Ein_G=[]
 	U = []
-	u = np.full(train.shape[0], 1 / train.shape[0])
+	u = np.full(trainX.shape[0], 1 / trainX.shape[0])
 	Eout_G=[]
 	for t in range(T):
-		g, ein, epsilon = get_g(train, u)
+		g, ein, epsilon = get_g(trainX, trainY, u)
 
 		Ein.append(ein)
 		
@@ -67,27 +67,32 @@ if __name__ == "__main__":
 		Ein_G.append(err_rate(trainX, trainY, G))
 		
 		U.append(np.sum(u))
-		u *= np.sqrt(1 / epsilon - 1)**(-trainY * g_func(g, trainX))
+
+		wrong_i = g_func(g, trainX) != trainY
+		correct_i = np.logical_not(wrong_i)
+		u[wrong_i] *= np.sqrt(1 / epsilon - 1)
+		u[correct_i] /= np.sqrt(1 / epsilon - 1)
+
 		Eout_G.append(err_rate(testX, testY, G))
 
 	#problem 13
 	print('Ein(gT) =',Ein[-1])
 	plt.plot(range(T), Ein)
-	draw(title='Problem 13',xlabel='t',ylabel='$E_{in}(g_t)$',savefig='13.jpg')
+	draw(title='Problem 13',xlabel='t',ylabel='$E_{in}(g_t)$')
 
 	#problem 14
 	print('Ein(GT) =',Ein_G[-1])
 	plt.plot(range(T), Ein_G)
-	draw(title='Problem 14',xlabel='t', ylabel='$E_{in}(G_t)$',savefig='14.jpg')
+	draw(title='Problem 14',xlabel='t', ylabel='$E_{in}(G_t)$')
 	
 	#problem 15
 	print('UT =', U[-1])
 	plt.plot(range(T), U)
-	draw(title='Problem 15',xlabel='t', ylabel='$U_t$',savefig='15.jpg')
+	draw(title='Problem 15',xlabel='t', ylabel='$U_t$')
 	
 	#problem 16
 	print('Eout(GT) =', Eout_G[-1])
 	#print(np.argmin(Eout_G))
 	plt.plot(range(T), Eout_G)
-	draw(title='Problem 16',xlabel='t', ylabel='$E_{out}(G_t)$',savefig='16.jpg')
+	draw(title='Problem 16',xlabel='t', ylabel='$E_{out}(G_t)$')
 	
