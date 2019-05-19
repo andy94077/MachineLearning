4078810f86b35be9202555008442e2db5cf1a363
andy94077 <andy94077@gmail.com>
2019-05-19 12:59:54 +0800

    [hw3] Fix predict function and eliminate redundant branching in DecisionTree.py

--- HW3/DecisionTree.py
+++ HW3/DecisionTree.py
@@ -19,43 +19,49 @@ class DecisionTree():
 				preorder.append(item.left)
 			else:
 				string += str(item) + '\n'
-		return string
+		return string[:-1] #no newline in the end
 	
-	def g_func(self, b, X):
-		return (b[0] * np.sign(X[:, b[1]] - b[2])).ravel()
-
-	def err_gini(self, b, X, Y):
-		result = self.g_func(b, X)
+	def g_func(self, X, b=None):
+		if b is None:
+			b = self.b
+		return (b[0] * np.sign(X[:, b[1]] - b[2])).astype(int).ravel()
+	
+	def __err_gini(self, b, X, Y):
+		result = self.g_func(X,b)
 		D = [Y[result == -1], Y[result == 1]]
 		return sum([(Dc.shape[0] - (np.sum(Dc==-1)**2+np.sum(Dc==1)**2)/Dc.shape[0]) for Dc in D if Dc.shape[0]!=0])
-
-	def find_b(self, X, Y):
+	
+	def __find_b(self, X, Y):
 		if np.all(Y == Y[0]):
 			return (int(Y[0]), 0, -np.inf),True  #i does not matter
 		elif np.all(X == X[0]):  #all X are the same
 			return (int(Y[0]), 0, -np.inf),True  #return garbage
 		else:
 			min_err = np.inf
-			min_err_b = (Y[0], 0, -np.inf)
+			min_err_b = (int(Y[0]), 0, -np.inf)
 			for i in range(X.shape[1]):
 				Xi = np.sort(X[:, i])
 				thetas = np.concatenate(([-np.inf], (Xi[:-1] + Xi[1:]) / 2))
 				for s in [-1, 1]:
 					for theta in thetas:
-						err = self.err_gini((s, i, theta), X, Y)
+						err = self.__err_gini((s, i, theta), X, Y)
 						if err < min_err:
 							min_err = err
 							min_err_b = (s, i, theta)
-			return min_err_b, min_err_b[2]==-np.inf # if min_err_b[2]==-np.inf, it means 'b' cannot split X into 2 partition
+			return min_err_b, min_err_b[2] == -np.inf # if min_err_b[2]==-np.inf, it means 'b' cannot split X into 2 partition
 							
 
 	def fit(self, X, Y):
-		self.b, end = self.find_b(X, Y)
+		self.b, end = self.__find_b(X, Y)
 		#print(self.b)
 		if not end:
-			result = self.g_func(self.b, X)
+			result = self.g_func(X)
 			self.left = DecisionTree().fit(X[result == -1], Y[result == -1])
+			if self.left.b[2] == -np.inf:
+				self.left = self.left.b[0]
 			self.right = DecisionTree().fit(X[result == 1], Y[result == 1])
+			if self.right.b[2] == -np.inf:
+				self.right = self.right.b[0]
 		self.fitted = True
 		return self
 
@@ -67,7 +73,7 @@ class DecisionTree():
 			for x in X:
 				node = self
 				while isinstance(node,DecisionTree):
-					node = node.left if node.g_func(self.b, x.reshape(1, -1)) == -1 else node.right
+					node = node.left if node.g_func(x.reshape(1, -1)) == -1 else node.right
 				result.append(node)
 			return np.array(result)
 	
